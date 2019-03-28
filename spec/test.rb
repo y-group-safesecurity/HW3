59e44a8e1251e07b75239a6d3bec357822081e69
kuenway <paul4952233@gmail.com>
2019-03-29 01:17:41 +0800

    part B done

--- spec/test.rb
+++ spec/test.rb
@@ -4,11 +4,40 @@ require_relative '../substitution_cipher'
 @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
 @key = 3
 
-p @cc.to_s[0]
+a = @cc.number.chars.select.each_with_index { |_, id| (id + 1) % 3 == 0 }
 
-enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-p enc.to_s
+b = @cc.number.chars.select.each_with_index { |_, id| (id + 1) % 3 != 0 }
 
-dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
-p dec.to_s
+tot = ''
 
+(@cc.number.length / 3 + 1).times do |x|
+  tot << a[x] if a[x].is_a? String
+  (@key - 1).times do |y|
+    tot << b[(@key - 1) * x + y] if b[(@key - 1) * x + y].is_a? String
+  end
+end
+
+# p tot
+
+enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
+p enc
+dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
+p dec
+
+
+# def enc_each(each_val, key)
+#     num1 = each_val.chars.select.each_with_index {|_, id| ((id + 1) % 3).zero?}
+#     num2 = each_val.chars.select.each_with_index {|_, id| !((id + 1) % 3).zero?}
+#     num_n = ''
+#     (each_val.length / 3 + 1).times do |x|
+#         num_n << num1[x] if num1[x].is_a? String
+#         (key - 1).times do |y|
+#             if num2[(key - 1) * x + y].is_a? String
+#                 num_n << num2[(key - 1) * x + y]
+#             end
+#          end
+#     end
+#     each_val = num_n
+# end
+
+# p enc_each(@cc.expiration_date,@key)
--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+
 
 module SubstitutionCipher
   # It's for Caeser
@@ -80,6 +80,58 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      num1 = document.number.chars.select.each_with_index { |_, id| ((id + 1) % key).zero?}
+      num2 = document.number.chars.select.each_with_index { |_, id| !((id + 1) % key).zero?}
+      num_n = ''
+      (document.number.length / 3 + 1).times do |x|
+        num_n << num1[x] if num1[x].is_a? String
+        (key - 1).times do |y|
+          if num2[(key - 1) * x + y].is_a? String
+            num_n << num2[(key - 1) * x + y]
+          end
+        end
+      end
+      document.number = num_n
+
+      exp1 = document.expiration_date.chars.select.each_with_index { |_, id| ((id + 1) % key).zero?}
+      exp2 = document.expiration_date.chars.select.each_with_index { |_, id| !((id + 1) % key).zero?}
+      exp_n = ''
+      (document.expiration_date.length / 3 + 1).times do |x|
+        exp_n << exp1[x] if exp1[x].is_a? String
+        (key - 1).times do |y|
+          if exp2[(key - 1) * x + y].is_a? String
+            exp_n << exp2[(key - 1) * x + y]
+          end
+        end
+      end
+      document.expiration_date = exp_n
+
+      own1 = document.owner.chars.select.each_with_index { |_, id| ((id + 1) % key).zero?}
+      own2 = document.owner.chars.select.each_with_index { |_, id| !((id + 1) % key).zero?}
+      own_n = ''
+      (document.owner.length / 3 + 1).times do |x|
+        own_n << own1[x] if own1[x].is_a? String
+        (key - 1).times do |y|
+          if own2[(key - 1) * x + y].is_a? String
+            own_n << own2[(key - 1) * x + y]
+          end
+        end
+      end
+      document.owner = own_n
+
+      crd1 = document.credit_network.chars.select.each_with_index { |_, id| ((id + 1) % key).zero?}
+      crd2 = document.credit_network.chars.select.each_with_index { |_, id| !((id + 1) % key).zero?}
+      crd_n = ''
+      (document.credit_network.length / 3 + 1).times do |x|
+        crd_n << crd1[x] if crd1[x].is_a? String
+        (key - 1).times do |y|
+          if crd2[(key - 1) * x + y].is_a? String
+            crd_n << crd2[(key - 1) * x + y]
+          end
+        end
+      end
+      document.credit_network = crd_n
+      document
     end
 
     # Decrypts String document using integer key
@@ -89,6 +141,58 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      num1 = document.number.chars.select.each_with_index { |_, id| (id + 1) % key == 1}
+      num2 = document.number.chars.select.each_with_index { |_, id| (id + 1) % key != 1}
+      num_n = ''
+      (document.number.length / 3 + 1).times do |x|
+        (key - 1).times do |y|
+          if num2[(key - 1) * x + y].is_a? String
+            num_n << num2[(key - 1) * x + y]
+          end
+        end
+        num_n << num1[x] if num1[x].is_a? String
+      end
+      document.number = num_n
+
+      exp1 = document.expiration_date.chars.select.each_with_index { |_, id| (id + 1) % key == 1}
+      exp2 = document.expiration_date.chars.select.each_with_index { |_, id| (id + 1) % key!= 1}
+      exp_n = ''
+      (document.expiration_date.length / 3 + 1).times do |x|
+        (key - 1).times do |y|
+          if exp2[(key - 1) * x + y].is_a? String
+            exp_n << exp2[(key - 1) * x + y]
+          end
+        end
+        exp_n << exp1[x] if exp1[x].is_a? String
+      end
+      document.expiration_date = exp_n
+
+      own1 = document.owner.chars.select.each_with_index { |_, id| (id + 1) % key == 1}
+      own2 = document.owner.chars.select.each_with_index { |_, id| (id + 1) % key != 1}
+      own_n = ''
+      (document.owner.length / 3 + 1).times do |x|
+        (key - 1).times do |y|
+          if own2[(key - 1) * x + y].is_a? String
+            own_n << own2[(key - 1) * x + y]
+          end
+        end
+        own_n << own1[x] if own1[x].is_a? String
+      end
+      document.owner = own_n
+
+      crd1 = document.credit_network.chars.select.each_with_index { |_, id| (id + 1) % key == 1}
+      crd2 = document.credit_network.chars.select.each_with_index { |_, id| (id + 1) % key != 1}
+      crd_n = ''
+      (document.credit_network.length / 3 + 1).times do |x|
+        (key - 1).times do |y|
+          if crd2[(key - 1) * x + y].is_a? String
+            crd_n << crd2[(key - 1) * x + y]
+          end
+        end
+        crd_n << crd1[x] if crd1[x].is_a? String
+      end
+      document.credit_network = crd_n
+      document.to_json
     end
   end
 end
